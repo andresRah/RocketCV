b4ad815b777f39cc6b6671b51cf618bfd3484f3f
Andres Arevalo <andres.leonardo@vorihealth.com>
2024-01-08 09:58:48 -0500

    Moved utils to the root project

---
+++ RocketCV.Utils/DynamicRoleAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketCV.Utils
+{
+    public class DynamicRoleRequirement : IAuthorizationRequirement
+    {
+        // Custom requirement properties and methods
+    }
+
+    public class DynamicRoleAuthorizationHandler : AuthorizationHandler<DynamicRoleRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, DynamicRoleRequirement requirement)
+        {
+            // Logic to validate the user's role
+            // You can access user claims through context.User
+
+            // Example: Check if the user has the "ADMIN" role
+            if (context.User.IsInRole("ADMIN"))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
---
+++ RocketCV.Utils/ValidateEntity.cs
@@ -0,0 +1,31 @@
+﻿namespace RocketCV.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Validate Entity
+    /// </summary>
+    public static class ValidateEntity
+    {
+        /// <summary>
+        /// Validates the specified entity.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="entity">The entity.</param>
+        /// <returns></returns>
+        public static IList<string> Validate<T>(this T entity) where T : class, new()
+        {
+            var contextValidator = new ValidationContext(entity);
+            var errors = new List<ValidationResult>();
+            var response = new List<string>();
+            Validator.TryValidateObject(entity, contextValidator, errors, true);
+            errors.ForEach(e => response.Add(e.ErrorMessage));
+            return response;
+        }
+    }
+}
